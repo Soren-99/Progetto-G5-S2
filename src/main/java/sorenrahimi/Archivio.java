87c81aa1353fec0c2298a0efb17ba93c9b30f8f4
Persiano99 <sorenrahimi99@gmail.com>
2024-03-15 14:14:03 +0100

    classe archivio

--- src/main/java/sorenrahimi/Archivio.java
+++ src/main/java/sorenrahimi/Archivio.java
@@ -5,5 +5,46 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Archivio {
+public class Archivio implements Serializable {
+    private List<Elemento> elementi;
+
+    public Archivio(){
+        this.elementi = new ArrayList<>();
+    }
+    public void aggiungiElemento(Elemento elemento) {
+        elementi.add(elemento);
+    }
+
+    public void rimuoviElemento(String isbn){
+        elementi.removeIf(e -> e.getIsbn().equals(isbn));
+    }
+    public Elemento ricercaPerISBN(String isbn){
+        return elementi.stream()
+                .filter(e -> e.getIsbn().equals(isbn))
+                .findFirst()
+                .orElse(null);
+    }
+    public  List<Elemento> ricercaPerAnnoPubblicazione(int anno){
+        return elementi.stream()
+                .filter(e -> e.getAnnoPubblicazione() == anno)
+                .collect(Collectors.toList());
+    }
+    public  List<Elemento> ricercaPerAutore(String autore){
+        return elementi.stream()
+                .filter(e -> e instanceof Libro && ((Libro) e).getAutore().equals(autore))
+                .collect(Collectors.toList());
+    }
+
+    public void salvataggioSuDisco(String filePath) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+        oos.writeObject(this);
+        }
+    }
+
+    public static Archivio caricamentoDalDisco(String filePath) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            return (Archivio) ois.readObject();
+    }
+
+    }
 }
